8fe8c97cb7148bb192dba6b8360c63d16577c8b0
jdsafhlfl <weiyi.xu@helsinki.fi>
2022-12-26 00:13:57 +0200

    exercise2.13

--- countries/src/App.js
+++ countries/src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [countryName, setName] = useState('')
   const [countryData, setData] = useState([])
   const [flag, setFlag] = useState(false)
+  const [singleCountryName, setSingle] = useState('')
+  const [showFlag, setShowFlag] = useState(false)
 
   useEffect(() => {
     axios.get('https://restcountries.com/v3.1/all')
@@ -17,48 +19,75 @@ function App() {
   const handleInput = (event) => {
     setName(event.target.value)
     setFlag(true)
+    setShowFlag(false)
   }
 
   const filterCountry = countryData.filter(country => country.name.common.toLowerCase().includes(countryName.toLowerCase()))
 
-  return (
-    <div>
+  const singleCountry = filterCountry.filter(country => country.name.common === singleCountryName)
+
+  if(showFlag === true){
+    return (
+      <div>
       find countries
       <input value={countryName} onChange={handleInput} />
-      <Filter flag={flag} country ={filterCountry} />
-    </div>
-  );
+      <FullInfor country={singleCountry} />
+      </div>
+    )
+  }
+  else{
+    return (
+      <div>
+        find countries
+        <input value={countryName} onChange={handleInput} />
+        <Filter flag={flag} country={filterCountry} update={setSingle} updateShow={setShowFlag} />
+      </div>
+    );
+  }
 }
 
-const Filter = (props) =>{
-  if(props.flag === true){
-    if(props.country.length > 10){
+const Filter = (props) => {
+  const show = (country_name) => {
+    props.update(country_name)
+    props.updateShow(true)
+  }
+
+  if (props.flag === true) {
+    if (props.country.length > 10) {
       return (
         <p>Too many matches, specify another filter</p>
       )
-    }else if(props.country.length === 1){
-      let language = []
-      for(var key in props.country[0].languages){
-        language.push(props.country[0].languages[key])
-      }
+    } else if (props.country.length === 1) {
       return (
-        <div>
-          <h1>{props.country[0].name.common}</h1>
-          <p>capital {props.country[0].capital[0]}</p>
-          <p>area {props.country[0].area}</p>
-          <h2>languages:</h2>
-          <ul>{language.map(l => <li key={l}>{l}</li>)}</ul>
-          <img src={props.country[0].flags.png} alt="flag"></img>
-        </div>
+        <FullInfor country={props.country} />
       )
-    }else{
+    } else {
       return (
         <div>
-          {props.country.map(c => <p key={c.name.common}>{c.name.common}</p>)}
+          {props.country.map(c => <p key={c.name.common}>{c.name.common} <button onClick={function () { show(c.name.common) }}>show</button></p>)}
         </div>
       )
     }
   }
 }
+
+const FullInfor = (props) => {
+  let language = []
+  for (var key in props.country[0].languages) {
+    language.push(props.country[0].languages[key])
+  }
+  return (
+    <div>
+      <h1>{props.country[0].name.common}</h1>
+      <p>capital {props.country[0].capital[0]}</p>
+      <p>area {props.country[0].area}</p>
+      <h2>languages:</h2>
+      <ul>{language.map(l => <li key={l}>{l}</li>)}</ul>
+      <img src={props.country[0].flags.png} alt="flag"></img>
+    </div>
+  )
+}
+
+
 
 export default App;
